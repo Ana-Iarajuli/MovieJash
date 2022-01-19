ed0650be62dd454adc8559265597277b82941b43
tsnini <nini.tsulaia.1@btu.edu.ge>
2022-01-19 22:32:37 +0400

    password reset activity + back

--- app/src/main/java/com/example/moviejash/PasswordReset.kt
+++ app/src/main/java/com/example/moviejash/PasswordReset.kt
@@ -1,11 +1,14 @@
 package com.example.moviejash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBar
+import com.example.moviejash.fragments_authentication.LoginFragment
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -13,6 +16,7 @@ class PasswordReset : AppCompatActivity() {
 
     private lateinit var ResetEmail2: EditText
     private lateinit var ResetPasswordButton2: Button
+    private lateinit var Back: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,7 @@ class PasswordReset : AppCompatActivity() {
 
         ResetEmail2 = findViewById(R.id.ResetEmail2)
         ResetPasswordButton2 = findViewById(R.id.ResetPasswordButton2)
+        Back = findViewById(R.id.Back)
 
         ResetPasswordButton2.setOnClickListener {
             val resetEmail = ResetEmail2.text.toString()
@@ -36,6 +41,10 @@ class PasswordReset : AppCompatActivity() {
                     Toast.makeText(this, "Enter valid Email", Toast.LENGTH_SHORT).show()
                 }
             }
+        }
+        Back.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
     }
 }
