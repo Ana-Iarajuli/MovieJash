2f7dc69feb92c54ff0d4b0dc89a75636ef8423ad
user2 <anna.iarajuli@gmail.com>
2022-01-19 17:27:55 +0400

    add bottom navigation

--- app/src/main/java/com/example/moviejash/bottom_nav_second.kt
+++ app/src/main/java/com/example/moviejash/bottom_nav_second.kt
@@ -2,10 +2,31 @@ package com.example.moviejash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class bottom_nav_second : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_nav_second)
+
+        val navView = findViewById<BottomNavigationView>(R.id.BottomNavigationView)
+
+        val controller = findNavController(R.id.nav_movies_fragment)
+
+        val appBarConfig = AppBarConfiguration(
+            setOf(
+                R.id.moviesFragment,
+                R.id.ticketsFragment,
+                R.id.profileFragment
+            )
+        )
+
+        setupActionBarWithNavController(controller, appBarConfig)
+        navView.setupWithNavController(controller)
+
     }
 }
