4f7118b264bf5089ebc73e9a9734a458ad595ee9
user2 <anna.iarajuli@gmail.com>
2022-01-21 04:12:09 +0400

    add editText in profile

--- app/src/main/java/com/example/moviejash/fragments_nav_bar/ProfileFragment.kt
+++ app/src/main/java/com/example/moviejash/fragments_nav_bar/ProfileFragment.kt
@@ -12,22 +12,18 @@ import com.example.moviejash.PasswordReset
 import com.example.moviejash.R
 
 class ProfileFragment: Fragment (R.layout.fragment_profile){
+
     private lateinit var ResPasswordButton: Button
     private lateinit var LogoutButton: Button
     private lateinit var UserPhoto: ImageView
-    private lateinit var Camera: Button
 
-    companion object{
-        val IMAGE_REQUEST_CODE = 100
 
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         ResPasswordButton = requireActivity().findViewById(R.id.ResPasswordButton)
         LogoutButton = requireActivity().findViewById(R.id.LogoutButton)
-        Camera = requireActivity().findViewById(R.id.Camera)
         UserPhoto = requireActivity().findViewById(R.id.UserPhoto)
 
 
@@ -40,24 +36,11 @@ class ProfileFragment: Fragment (R.layout.fragment_profile){
             startActivity(Intent(requireActivity(), MainActivity::class.java))
             requireActivity().finish()
         }
-        Camera.setOnClickListener {
-            pickImageGallery()
-        }
+
+
 
 
     }
 
-    private fun pickImageGallery() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        requireActivity().startActivityForResult(intent, IMAGE_REQUEST_CODE)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK){
-            UserPhoto.setImageURI(data?.data)
-        }
-    }
 
 }
